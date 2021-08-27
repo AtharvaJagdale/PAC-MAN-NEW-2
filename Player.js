5e14c9c93bb306fe52c87253f643d8b0bc96f11c
AtharvaJagdale <83119039+AtharvaJagdale@users.noreply.github.com>
2021-08-27 09:02:46 +0530

    Add files via upload

--- Player.js
+++ Player.js
@@ -3,7 +3,7 @@ class Player {
 constructor()
 {
 
-    this.body=createSprite(100,100,100,100)
+    this.body=createSprite(350,620,100,100)
     this.Image=loadAnimation("images/Pacman1.png","images/pacman2.png")
     this.body.addAnimation("Pacman",this.Image)
     this.body.scale=0.2
--- sketch.js
+++ sketch.js
@@ -10,18 +10,27 @@ function preload(){
 function setup() {
   createCanvas(800,800);
   pacman1=new Player()
-  Wall1=new Wall(796,500,400,30)
-  Wall2=new Wall(700,400,30,200)
-  Wall3=new Wall(380,500,300,30)
-  Wall4=new Wall(520,590,30,200)
-  Wall5=new Wall(790,145,300,30)
-  Wall6=new Wall(750,145,300,30)
-  Wall7=new Wall(5,140,350,30)
-  Wall8=new Wall(250,250,30,250)
-  Wall9=new Wall(360,370,250,30)
-  Wall10=new Wall(445,80,30,300)
-  Wall11=new Wall(50,440,300,30)
-  Wall12=new Wall(5,220,300,30)
+  Wall1=new Wall(230,205,200,20)
+  Wall2=new Wall(500,205,200,20)
+  Wall3=new Wall(450,350,20,100)
+  Wall4=new Wall(280,350,20,100)
+  Wall5=new Wall(360,390,170,20)
+  Wall6=new Wall(230,490,200,20)
+  Wall7=new Wall(500,490,200,20)
+  Wall8=new Wall(75,330,20,150)
+  Wall9=new Wall(115,400,100,20)
+  Wall10=new Wall(670,330,20,150)
+  Wall11=new Wall(630,400,100,20)
+  Wall12=new Wall(160,655,200,20)
+  Wall13=new Wall(260,615,20,100)
+  Wall14=new Wall(350,565,200,20)
+  Wall15=new Wall(450,605,20,100)
+  Wall16=new Wall(540,650,200,20)
+  Wall17=new Wall(160,70,200,20)
+  Wall18=new Wall(260,85,20,50)
+  Wall19=new Wall(350,110,200,20)
+  Wall20=new Wall(450,95,20,50)
+  Wall21=new Wall(540,70,200,20)
 }
 
 function draw() {
@@ -46,4 +55,6 @@ function draw() {
         pacman1.body.y+=5
         pacman1.body.rotation=90
         }
+
+ 
 }
